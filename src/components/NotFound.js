d10eeaccbd72db29e779aa3d25ebd3c0bef4856f
gitfilix <runlevel07@gmail.com>
2019-09-28 19:28:24 +0200

    simple fileRouter implemented

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => (
+  <React.Fragment>
+    <h2>Not Found ? thids site schould nevr bee on your schreeen!</h2>
+  </React.Fragment>
+)
+
+export default NotFound
